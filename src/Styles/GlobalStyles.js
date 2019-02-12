72750bc8c4b0c6763555c1f9cf0e284aef9b52d2
Jane Lee <Yesdoing13@gmail.com>
2019-02-12 16:21:07 +0900

    add padding-top

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -13,6 +13,7 @@ const GlobalStyles = createGlobalStyle`
     body {
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size: 14px;
+        padding-top: 85px;
     }
 `;
 
