b1d8d6fbf7201554235fee6ad7f90bd85a0dd459
Jane Lee <Yesdoing13@gmail.com>
2019-02-14 21:05:24 +0900

    refactor: db logic

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,33 @@
-import {list as richWebList, chat as richWebChat, talk as richWebTalk} from 'lib/db';
+import moment from "moment";
+import {
+  chatList,
+  chatDetail,
+  addMessage,
+  updateMessageAllRead
+} from "lib/db";
 
 export const richWebApi = {
-    list: () => richWebList(),
-    chat: (index) => richWebChat(index),
-    talk: (index, message) => richWebTalk(index, message)
+  chatList: () => {
+    const lists = chatList();
+    const response = lists.map(item => ({
+      id: item.id,
+      username: item.from_user.username,
+      user_profile_image: item.from_user.user_profile_image,
+      recent_message: item.messages[item.messages.length - 1],
+      unread_number:  item.messages.reduce((acc, cur) => {
+        if(cur.is_read === "false") return acc + 1;
+        return acc;
+      }, 0) 
+    }));
+    response.sort((a, b) =>
+      moment
+        .utc(b.recent_message.date, "YYYY MMM DD,ddd,hh:mm:ss")
+        .diff(moment.utc(a.recent_message.date, "YYYY MMM DD,ddd,hh:mm:ss"))
+    );
+    console.log(response);
+    return response;
+  },
+  chatDetail: index => chatDetail(index),
+  addMessage: (index, message) => addMessage(index, message),
+  updateMessageAllRead: (index)=> updateMessageAllRead(index)
 };
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,284 +1,271 @@
-
 const talks = [
-    { 
-        "id": 0,
-        "to_user": {
-            "username": "yesdoing",
-            "user_profile_image": "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
-        },
-        "from_user": {
-            "username": "임블리",
-            "user_profile_image": "https://cf.shop.s.zigzag.kr/images/imvely.jpg"
-        },
-        "messages":[
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"Hey, How's it going this week?"
-            },
-            {
-                "username":"yesdoing",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content": "Haha that's was so funny Story hahahahahahahahahahahahahaha"
-            },
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"Hey, how it is going?"
-            },
-    ]},
-    {
-        "id": 1,
-        "to_user": {
-            "username": "yesdoing",
-            "user_profile_image": "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
-        },
-        "from_user": {
-            "username": "미아마스빈",
-            "user_profile_image": "https://cf.shop.s.zigzag.kr/images/miamasvin.jpg",
-        },
-        "messages":[
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"Hey, How's it going this week?"
-            },
-            {
-                "username":"yesdoing",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"졸려졸려"
-            },
-            {
-                "username":"임블리",
-                "date":"2019 Feb 08,Fri,11:20",
-                "content": "Haha that's was so funny Story hahahahahahahahahahahahahaha"
-            },
-    ]},
-    { 
-        "id": 2,
-        "to_user": {
-            "username": "yesdoing",
-            "user_profile_image": "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
-        },
-        "from_user": {
-            "username": "모코블링",
-            "user_profile_image": "https://cf.shop.s.zigzag.kr/images/mocobling.jpg",
-        },
-        "messages":[
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"Hey, How's it going this week?"
-            },
-            {
-                "username":"yesdoing",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"졸려졸려"
-            },
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"Hey, How's it going this week?"
-            }
-    ]},
-    { 
-        "id": 3,
-        "to_user": {
-            "username": "yesdoing",
-            "user_profile_image": "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
-        },
-        "from_user": {
-            "username": "모코블링",
-            "user_profile_image": "https://cf.shop.s.zigzag.kr/images/mocobling.jpg",
-        },
-        "messages":[
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"Hey, How's it going this week?"
-            },
-            {
-                "username":"yesdoing",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"졸려졸려"
-            },
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content": "Haha that's was so funny Story hahahahahahahahahahahahahaha"
-            }        
-    ]},
-    { 
-        "id": 4,
-        "to_user": {
-            "username": "yesdoing",
-            "user_profile_image": "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
-        },
-        "from_user": {
-            "username": "모코블링",
-            "user_profile_image": "https://cf.shop.s.zigzag.kr/images/mocobling.jpg",
-        },
-        "messages":[
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"Hey, How's it going this week?"
-            },
-            {
-                "username":"yesdoing",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"졸려졸려"
-            },
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content": "Haha that's was so funny Story hahahahahahahahahahahahahaha"
-            }        
-    ]},
-    { 
-        "id": 5,
-        "to_user": {
-            "username": "yesdoing",
-            "user_profile_image": "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
-        },
-        "from_user": {
-            "username": "모코블링",
-            "user_profile_image": "https://cf.shop.s.zigzag.kr/images/mocobling.jpg",
-        },
-        "messages":[
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"Hey, How's it going this week?"
-            },
-            {
-                "username":"yesdoing",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"졸려졸려"
-            },
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content": "Haha that's was so funny Story hahahahahahahahahahahahahaha"
-            }        
-    ]},
-    { 
-        "id": 6,
-        "to_user": {
-            "username": "yesdoing",
-            "user_profile_image": "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
-        },
-        "from_user": {
-            "username": "모코블링",
-            "user_profile_image": "https://cf.shop.s.zigzag.kr/images/mocobling.jpg",
-        },
-        "messages":[
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"Hey, How's it going this week?"
-            },
-            {
-                "username":"yesdoing",
-                "date":"2016-11-16 22:32:17:48",
-                "content":"졸려졸려"
-            },
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content": "Haha that's was so funny Story hahahahahahahahahahahahahaha"
-            }        
-    ]},
-    { 
-        "id": 7,
-        "to_user": {
-            "username": "yesdoing",
-            "user_profile_image": "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
-        },
-        "from_user": {
-            "username": "모코블링",
-            "user_profile_image": "https://cf.shop.s.zigzag.kr/images/mocobling.jpg",
-        },
-        "messages":[
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"Hey, How's it going this week?"
-            },
-            {
-                "username":"yesdoing",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content":"졸려졸려"
-            },
-            {
-                "username":"임블리",
-                "date":"2019 Feb 12,Tue,11:20",
-                "content": "Haha that's was so funny Story hahahahahahahahahahahahahaha"
-            }        
-    ]},
-    ];
-
-const chatList = [
-    {
-        id: 0,
+  {
+    id: 0,
+    to_user: {
+      username: "yesdoing",
+      user_profile_image:
+        "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
+    },
+    from_user: {
+      username: "임블리",
+      user_profile_image: "https://cf.shop.s.zigzag.kr/images/imvely.jpg"
+    },
+    messages: [
+      {
         username: "임블리",
-        user_profile_image: "https://cf.shop.s.zigzag.kr/images/imvely.jpg",
-        recent_message: talks[0].messages[talks[0].messages.length-1]
+        date: "2019 Feb 12,Tue,11:20:12",
+        content: "Hey, How's it going this week?",
+        is_read: "false"
+      },
+      {
+        username: "yesdoing",
+        date: "2019 Feb 12,Tue,11:20:14",
+        content: "Haha that's was so funny Story hahahahahahahahahahahahahaha",
+        is_read: "false"
+      },
+      {
+        username: "임블리",
+        date: "2019 Feb 12,Tue,11:20:27",
+        content: "Hey, how it is going?",
+        is_read: "false"
+      }
+    ]
+  },
+  {
+    id: 1,
+    to_user: {
+      username: "yesdoing",
+      user_profile_image:
+        "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
     },
-    {
-        id: 1,
+    from_user: {
+      username: "미아마스빈",
+      user_profile_image: "https://cf.shop.s.zigzag.kr/images/miamasvin.jpg"
+    },
+    messages: [
+      {
         username: "미아마스빈",
-        user_profile_image: "https://cf.shop.s.zigzag.kr/images/miamasvin.jpg",
-        recent_message: talks[1].messages[talks[1].messages.length-1]
+        date: "2019 Feb 12,Tue,11:20:12",
+        content: "Hey, How's it going this week?",
+        is_read: "true"
+      },
+      {
+        username: "yesdoing",
+        date: "2019 Feb 12,Tue,11:20:42",
+        content: "졸려졸려",
+        is_read: "true"
+      },
+      {
+        username: "미아마스빈",
+        date: "2019 Feb 08,Fri,11:20:54",
+        content: "Haha that's was so funny Story hahahahahahahahahahahahahaha",
+        is_read: "false"
+      }
+    ]
+  },
+  {
+    id: 2,
+    to_user: {
+      username: "yesdoing",
+      user_profile_image:
+        "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
     },
-    {
-        id: 2,
+    from_user: {
+      username: "모코블링",
+      user_profile_image: "https://cf.shop.s.zigzag.kr/images/mocobling.jpg"
+    },
+    messages: [
+      {
         username: "모코블링",
-        user_profile_image: "https://cf.shop.s.zigzag.kr/images/mocobling.jpg",
-        recent_message: talks[2].messages[talks[2].messages.length-1]
+        date: "2019 Feb 12,Tue,11:20:01",
+        content: "Hey, How's it going this week?",
+        is_read: "true"
+      },
+      {
+        username: "yesdoing",
+        date: "2019 Feb 12,Tue,11:20:04",
+        content: "졸려졸려",
+        is_read: "true"
+      },
+      {
+        username: "모코블링",
+        date: "2019 Feb 12,Tue,11:20:06",
+        content: "Hey, How's it going this week?",
+        is_read: "false"
+      }
+    ]
+  },
+  {
+    id: 3,
+    to_user: {
+      username: "yesdoing",
+      user_profile_image:
+        "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
     },
-    {
-        id: 3,
+    from_user: {
+      username: "데일리먼데이",
+      user_profile_image: "https://cf.shop.s.zigzag.kr/images/dailymonday.jpg"
+    },
+    messages: [
+      {
         username: "데일리먼데이",
-        user_profile_image: "https://cf.shop.s.zigzag.kr/images/dailymonday.jpg",
-        recent_message: talks[3].messages[talks[3].messages.length-1]
+        date: "2019 Feb 12,Tue,11:21:25",
+        content: "Hey, How's it going this week?"
+      },
+      {
+        username: "yesdoing",
+        date: "2019 Feb 12,Tue,11:22:22",
+        content: "졸려졸려",
+        is_read: "true"
+      },
+      {
+        username: "데일리먼데이",
+        date: "2019 Feb 12,Tue,11:23:33",
+        content: "Haha that's was so funny Story hahahahahahahahahahahahahaha",
+        is_read: "true"
+      }
+    ]
+  },
+  {
+    id: 4,
+    to_user: {
+      username: "yesdoing",
+      user_profile_image:
+        "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
     },
-    {
-        id: 4,
+    from_user: {
+      username: "파티수",
+      user_profile_image: "https://cf.shop.s.zigzag.kr/images/partysu.jpg"
+    },
+    messages: [
+      {
         username: "파티수",
-        user_profile_image: "https://cf.shop.s.zigzag.kr/images/partysu.jpg",
-        recent_message: talks[4].messages[talks[4].messages.length-1]
+        date: "2019 Feb 12,Tue,11:24:55",
+        content: "Hey, How's it going this week?",
+        is_read: "true"
+      },
+      {
+        username: "yesdoing",
+        date: "2019 Feb 12,Tue,11:30:00",
+        content: "졸려졸려",
+        is_read: "true"
+      },
+      {
+        username: "파티수",
+        date: "2019 Feb 12,Tue,11:31:11",
+        content: "Haha that's was so funny Story hahahahahahahahahahahahahaha",
+        is_read: "true"
+      }
+    ]
+  },
+  {
+    id: 5,
+    to_user: {
+      username: "yesdoing",
+      user_profile_image:
+        "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
     },
-    {
-        id: 5,
+    from_user: {
+      username: "하나언니",
+      user_profile_image: "https://cf.shop.s.zigzag.kr/images/hanaunni.jpg"
+    },
+    messages: [
+      {
         username: "하나언니",
-        user_profile_image: "https://cf.shop.s.zigzag.kr/images/hanaunni.jpg",
-        recent_message: talks[5].messages[talks[5].messages.length-1]
+        date: "2019 Feb 12,Tue,11:11:11",
+        content: "Hey, How's it going this week?",
+        is_read: "true"
+      },
+      {
+        username: "yesdoing",
+        date: "2019 Feb 12,Tue,11:12:22",
+        content: "졸려졸려",
+        is_read: "true"
+      },
+      {
+        username: "하나언니",
+        date: "2019 Feb 12,Tue,11:20:22",
+        content: "Haha that's was so funny Story hahahahahahahahahahahahahaha",
+        is_read: "true"
+      }
+    ]
+  },
+  {
+    id: 6,
+    to_user: {
+      username: "yesdoing",
+      user_profile_image:
+        "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
     },
-    {
-        id: 6,
+    from_user: {
+      username: "육육걸즈",
+      user_profile_image: "https://cf.shop.s.zigzag.kr/images/66girls.jpg"
+    },
+    messages: [
+      {
         username: "육육걸즈",
-        user_profile_image: "https://cf.shop.s.zigzag.kr/images/66girls.jpg",
-        recent_message: talks[6].messages[talks[6].messages.length-1]
+        date: "2019 Feb 12,Tue,11:23:33",
+        content: "Hey, How's it going this week?",
+        is_read: "true"
+      },
+      {
+        username: "yesdoing",
+        date: "2019 Feb 12,Tue,11:23:43",
+        content: "졸려졸려",
+        is_read: "true"
+      },
+      {
+        username: "육육걸즈",
+        date: "2019 Feb 12,Tue,11:44:44",
+        content: "Haha that's was so funny Story hahahahahahahahahahahahahaha",
+        is_read: "false"
+      }
+    ]
+  },
+  {
+    id: 7,
+    to_user: {
+      username: "yesdoing",
+      user_profile_image:
+        "https://thumb.velog.io/resize?url=https://images.velog.io/profiles/yesdoing/thumbnails/1538470361.899.png&width=256"
     },
-    {
-        id: 7,
+    from_user: {
+      username: "핫핑",
+      user_profile_image: "https://cf.shop.s.zigzag.kr/images/hotping.jpg"
+    },
+    messages: [
+      {
         username: "핫핑",
-        user_profile_image: "https://cf.shop.s.zigzag.kr/images/hotping.jpg",
-        recent_message: talks[7].messages[talks[7].messages.length-1]
-    },
-]
+        date: "2019 Feb 12,Tue,11:20:11",
+        content: "Hey, How's it going this week?",
+        is_read: "false"
+      },
+      {
+        username: "yesdoing",
+        date: "2019 Feb 12,Tue,11:22:11",
+        content: "졸려졸려",
+        is_read: "false"
+      },
+      {
+        username: "핫핑",
+        date: "2019 Feb 12,Tue,11:23:00",
+        content: "Haha that's was so funny Story hahahahahahahahahahahahahaha",
+        is_read: "false"
+      }
+    ]
+  }
+];
 
-export const list = () => ({
-    list: chatList
-});
+export const chatList = () => talks;
 
-
-
-export const chat = (index) => {
-    return talks[index];
+export const chatDetail = index => {
+  return talks[index];
 };
 
-export const talk = (index, message) => {
-    talks[index].messages.push(message);
+export const addMessage = (index, message) => {
+  talks[index].messages.push(message);
 };
+
+export const updateMessageAllRead = (index) => {
+  talks[index].messages.forEach(item => item.is_read = "true")
+}
